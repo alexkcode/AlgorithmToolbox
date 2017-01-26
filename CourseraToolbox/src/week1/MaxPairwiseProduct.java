27bf02a796180cad6cfb0b6d31d3b3801b164239
Alexis Kwan <alexiskwan.uiuc@gmail.com>
2017-01-26 11:58:33 -0700

    implemented faster version of max pairwise product method

--- CourseraToolbox/src/week1/MaxPairwiseProduct.java
+++ CourseraToolbox/src/week1/MaxPairwiseProduct.java
@@ -2,9 +2,12 @@ import java.util.*;
 import java.io.*;
 
 public class MaxPairwiseProduct {
-    static int getMaxPairwiseProduct(int[] numbers) {
+//    changed types of methods and input to account for larger numbers
+//    as indicated in exercise
+
+    private static long getMaxPairwiseProduct(long[] numbers) {
 //        naive solution
-        int result = 0;
+        long result = 0;
         int n = numbers.length;
         for (int i = 0; i < n; ++i) {
             for (int j = i + 1; j < n; ++j) {
@@ -16,14 +19,32 @@ public class MaxPairwiseProduct {
         return result;
     }
 
+    private static long getMaxPairwiseProductFast(long[] numbers) {
+//        faster solution
+        int n = numbers.length;
+
+        int max_index1 = -1;
+        for (int i = 0; i < n; ++i)
+            if ((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
+                max_index1 = i;
+
+        int max_index2 = -1;
+        for (int j = 0; j < n; ++j)
+            if ((numbers[j] != numbers[max_index1]) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+                max_index2 = j;
+
+        return numbers[max_index1] * numbers[max_index2];
+    }
+
     public static void main(String[] args) {
         FastScanner scanner = new FastScanner(System.in);
         int n = scanner.nextInt();
-        int[] numbers = new int[n];
+        long[] numbers = new long[n];
         for (int i = 0; i < n; i++) {
             numbers[i] = scanner.nextInt();
         }
         System.out.println(getMaxPairwiseProduct(numbers));
+        System.out.println(getMaxPairwiseProductFast(numbers));
     }
 
     static class FastScanner {
