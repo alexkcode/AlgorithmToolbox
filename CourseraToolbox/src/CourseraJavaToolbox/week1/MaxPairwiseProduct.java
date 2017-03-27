d6112e503b97cef304bd91b084d16fb91489a0a3
alexkcode <alexiskwan.uiuc@gmail.com>
2017-03-26 21:35:22 -0500

    refactored performance test

--- CourseraToolbox/src/CourseraJavaToolbox/week1/MaxPairwiseProduct.java
+++ CourseraToolbox/src/CourseraJavaToolbox/week1/MaxPairwiseProduct.java
@@ -39,22 +39,14 @@ public class MaxPairwiseProduct {
     }
 
     public static void main(String[] args) {
-//        FastScanner scanner = new FastScanner(System.in);
-//        int n = scanner.nextInt();
-//        ArrayList<Integer> numbers = new ArrayList<Integer>();
-//        for (int i = 0; i < n; ++i) {
-//            numbers.add(i, scanner.nextInt());
-//        }
-//        System.out.println(getMaxPairwiseProduct(numbers));
-//        System.out.println(getMaxPairwiseProductFast(numbers));
-        try{
-            equivalencyTest();
-            integerOverflowTest();
-            timeLimitTest();
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        for (int i = 0; i < n; ++i) {
+            numbers.add(i, scanner.nextInt());
         }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+        System.out.println(getMaxPairwiseProduct(numbers));
+        System.out.println(getMaxPairwiseProductFast(numbers));
     }
 
     static class FastScanner {
@@ -85,52 +77,4 @@ public class MaxPairwiseProduct {
         }
     }
 
-    public static ArrayList<Integer> generateRandNumbers(Integer arraySize, Integer elementSize) throws Exception {
-//        int n = new Random().nextInt(arraySize) + 2;
-
-        ArrayList<Integer> arr = new ArrayList<>();
-        for (int i=0; i<arraySize; i++){
-            arr.add(new Random().nextInt(elementSize));
-        }
-        return arr;
-    }
-
-    public static void equivalencyTest() throws Exception {
-        ArrayList<Integer> arr = new ArrayList<>();
-        long test1 = MaxPairwiseProduct.getMaxPairwiseProduct(arr);
-        long test2 = MaxPairwiseProduct.getMaxPairwiseProductFast(arr);
-
-        System.out.println(String.format("Product1 : %1$d\nProduct2 : %2$d", test1, test2));
-
-        if (test1 != test2){
-            System.out.println("Pairwise products are not equal!");
-        }
-    }
-
-    public static void integerOverflowTest() throws Exception {
-        ArrayList<Integer> arr = new ArrayList<>();
-        long test1 = MaxPairwiseProduct.getMaxPairwiseProduct(arr);
-        long test2 = MaxPairwiseProduct.getMaxPairwiseProductFast(arr);
-
-        System.out.println(String.format("Product1 : %1$d\nProduct2 : %2$d", test1, test2));
-
-        if (test1 != test2){
-            System.out.println("Pairwise products are not equal!");
-        }
-    }
-
-    public static void timeLimitTest() throws Exception {
-        ArrayList<Integer> arr = generateRandNumbers(200000, 100);
-        Date time1 = new Date();
-        long test = MaxPairwiseProduct.getMaxPairwiseProductFast(arr);
-        Date time2 = new Date();
-        long timeDel = time2.getTime() - time1.getTime();
-
-        System.out.println(String.format("Product1 : %1$d\nProduct2 : %2$d", time1, time2));
-        System.out.println(String.format("Time Delta : %1d", timeDel));
-
-        if (timeDel > 1000000) {
-            System.out.println("Pairwise product time limit test failed!");
-        }
-    }
 }
